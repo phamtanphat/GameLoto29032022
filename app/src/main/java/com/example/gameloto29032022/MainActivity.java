4df5c8561b161c6d0d7fe209db4edcb42e03453e
Phat <phatdroid94@gmail.com>
2022-05-07 18:38:40 +0700

    finish

--- app/src/main/java/com/example/gameloto29032022/MainActivity.java
+++ app/src/main/java/com/example/gameloto29032022/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,20 +34,53 @@ public class MainActivity extends AppCompatActivity {
         mRandom = new Random();
 
         //Thêm số vào mảng
-        for (int i = 1; i <= 100; i++) {
-            mArrNumbers.add(i);
-        }
+        addNumberInArray();
 
+        // Unit test
+        // Case 1 : Các giá trị không được trùng
+        // Case 2 : Nếu như hết số random thì thông báo
+        // Case 3 (option) : Hiển thị không bị dư dấu -
+        // Case 4 : Button reset sẽ xóa lịch sử , out put và thêm lại tất cả số
         mBtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int index = mRandom.nextInt(mArrNumbers.size());
-                int value = mArrNumbers.get(index);
-                mTvOutput.setText(value + "");
-                mTextResult = value + " - " + mTextResult;
-                mTvHistory.setText(mTextResult);
+                if (mArrNumbers.size() > 0){
+                    int index = mRandom.nextInt(mArrNumbers.size());
+                    int value = mArrNumbers.get(index);
+                    mTvOutput.setText(value + "");
+                    if (mArrNumbers.size() == 100){
+                        mTextResult += value ;
+                    }else{
+                        mTextResult = value + " - " + mTextResult;
+                    }
+                    mTvHistory.setText(mTextResult);
+                    mArrNumbers.remove(index);
+                }else{
+                    Toast.makeText(MainActivity.this, "Kết thúc!!!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mArrNumbers.size() > 0){
+                    mArrNumbers.clear();
+                }
+                addNumberInArray();
+                mTextResult = "";
+                mTvOutput.setText("");
+                mTvHistory.setText("");
+
+            }
+        });
+    }
+
+    private void addNumberInArray(){
+        for (int i = 1; i <= 100; i++) {
+            mArrNumbers.add(i);
+        }
 
     }
 }
